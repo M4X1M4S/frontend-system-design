a9acc2146a729a1f82b81e9e4c2ef8ee75639b53
ABHILASH.. <99235816+M4X1M4S@users.noreply.github.com>
2025-05-10 18:56:19 +0530

    Add CRUD operations

--- Notes/Networking/RestApi/index.js
+++ Notes/Networking/RestApi/index.js
@@ -1,7 +1,59 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 3000;
+const todos = [
+  {
+    id: "1",
+    name: "Todo 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    name: "Todo 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    name: "Todo 3",
+    completed: true,
+  },
+];
+app.use(bodyParser.json());
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json({
+    message: "Todo added successfully",
+  });
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
+  if (todoIndex !== -1) {
+    todos[todoIndex] = { ...req.body, id };
+    res.json({
+      message: "Todo updated successfully",
+    });
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    res.json({
+      message: "Todo deleted successfully",
+    });
+  }
+});
 app.all("/", (req, res) => {
   res.send("Hello World!");
   console.log("REQUEST>", req);
